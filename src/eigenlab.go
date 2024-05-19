35d8ff269491ee8e5f6a3dcb6b0ec6686ad8fdb1
weiclw <92598652+weiclw@users.noreply.github.com>
2024-05-18 21:30:39 -0700

    First module: run qemu, support auto-run via environment variable.

---
+++ src/eigenlab.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "os/exec"
+    "time"
+)
+
+func getCmdsReal() []string {
+    cmdlines := []string{
+        "/opt/homebrew/bin/qemu-system-aarch64",
+        "-L",
+        "/Applications/UTM.app/Contents/Resources/qemu",
+        "-cpu",
+        "host",
+        "-smp",
+        "cpus=2,sockets=1,cores=2,threads=1",
+        "-machine",
+        "virt,",
+        "-accel",
+        "hvf",
+        "-accel",
+        "tcg,tb-size=512",
+        "-drive",
+        "if=pflash,format=raw,unit=0,file=/Applications/UTM.app/Contents/Resources/qemu/edk2-aarch64-code.fd,readonly=on",
+        "-drive",
+        "if=pflash,unit=1,file=/Applications/UTM.app/Contents/Resources/qemu/edk2-arm-vars.fd",
+        "-nographic",
+        "-boot",
+        "menu=on",
+        "-m",
+        "2048",
+        "-device",
+        "intel-hda",
+        "-device",
+        "virtio-blk-pci,drive=drive0,bootindex=0",
+        "-drive",
+        "if=none,media=disk,id=drive0,file=/tmp/alpine-virt-3.18.4-aarch64.iso"}
+
+    return cmdlines
+}
+
+func issueCommand(wr *io.PipeWriter, delay time.Duration, prompt string, cmd []byte) {
+    time.Sleep(delay)
+    fmt.Println(prompt)
+    _, err := wr.Write(cmd)
+    if err != nil {
+        fmt.Println("Failed to write to pipe: ", err)
+    } else {
+        fmt.Println("Successfully write the command")
+    }
+}
+
+func readActionFile(path string) ([]string, error) {
+     file, err := os.Open(path)
+     if err != nil {
+         fmt.Println("failed to read from file: ", path)
+         return []string{}, err
+     }
+
+     defer file.Close()
+     actions := []string{}
+     scanner := bufio.NewScanner(file)
+     for scanner.Scan() {
+         actions = append(actions, scanner.Text())
+     }
+
+     return actions, nil
+}
+
+func asyncInputs(no_redirect bool, action_file string, wr *io.PipeWriter) {
+    defer wr.Close()
+
+    // No need to continue if auto-run is not needed.
+    if no_redirect {
+        return
+    }
+
+    cmd_list, read_err := readActionFile(action_file)
+    if read_err != nil {
+        return
+    }
+
+    // Wait for initial prompt.
+    time.Sleep(10*1000*time.Millisecond)
+
+    for i := 0; i < len(cmd_list); i++ {
+        issueCommand(wr, 2*1000*time.Millisecond, "Issue command", []byte(cmd_list[i]))
+    }
+}
+
+func runCmds(x []string) {
+     remainings := x[1:]
+     cmd := exec.Command(x[0], remainings...)
+
+     action_file := os.Getenv("qemu_action_file")
+     _, no_redirect := os.LookupEnv("qemu_no_redirect")
+     if no_redirect {
+         fmt.Println("Do not redirect input")
+     }
+
+     rd, wr := io.Pipe()
+     defer rd.Close()
+
+     cmd.Stdout = os.Stdout
+     cmd.Stderr = os.Stderr
+
+     if no_redirect {
+         cmd.Stdin = os.Stdin
+     } else {
+         cmd.Stdin = rd
+     }
+
+     fmt.Println("Before launching go routine")
+     go asyncInputs(no_redirect, action_file, wr)
+
+     err := cmd.Run()
+
+     if err != nil {
+         fmt.Println("Something goes wrong: ", err)
+     }
+}
+
+func main() {
+    args := getCmdsReal()
+    runCmds(args)
+}
